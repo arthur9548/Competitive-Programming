4f6fde4e229dceb15924cac8f95548638fcef35b
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-01-20 13:09:39 -0300

    Updated template.cpp

--- Code/Misc/template.cpp
+++ Code/Misc/template.cpp
@@ -1,1 +1,37 @@
-#
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define float long double
+#define esp ' '
+#define endl '\n'
+#define YES "YES\n"
+#define NO "NO\n"
+#define vi vector<int>
+#define matrix vector<vector<int>>
+#define pii pair<int, int>
+#define pb push_back
+#define ppb pop_back
+#define all(x) x.begin(), x.end()
+#define rep(i, a, b) for(int i = (int)(a); i < (int)(b); i++)
+#define repstep(i, a, b, s) for(int i = (int)(a); i < (int)(b); i+=(s))
+#define repinv(i, a, b) for(int i = (int)(a); i >= (int)(b); i--)
+#define debug(var) cout << #var << ": " << var << endl
+#define sz(x) (int)x.size()
+
+constexpr int oo = (((unsigned int)-1)>>4);
+constexpr int MOD = 1e9+7;
+constexpr int MXN = 2e5+2;
+
+void solve(){
+	int n;
+}
+
+signed main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int tests=1;
+	//cin>>t;
+	while(tests--)solve();
+}
+
+
+
