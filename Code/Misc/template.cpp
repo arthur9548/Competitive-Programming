cfd43bec5caaacea87f3ead1ef8122c75cc2494b
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-01-27 16:40:01 -0300

    Updated template.cpp

--- Code/Misc/template.cpp
+++ Code/Misc/template.cpp
@@ -6,11 +6,13 @@ using namespace std;
 #define endl '\n'
 #define YES "YES\n"
 #define NO "NO\n"
+#define IMP "IMPOSSIBLE\n"
 #define vi vector<int>
 #define matrix vector<vector<int>>
 #define pii pair<int, int>
 #define pb push_back
 #define ppb pop_back
+#define eb emplace_back
 #define all(x) x.begin(), x.end()
 #define rep(i, a, b) for(int i = (int)(a); i < (int)(b); i++)
 #define repstep(i, a, b, s) for(int i = (int)(a); i < (int)(b); i+=(s))
@@ -21,6 +23,7 @@ using namespace std;
 constexpr int oo = (((unsigned int)-1)>>4);
 constexpr int MOD = 1e9+7;
 constexpr int MXN = 2e5+2;
+constexpr int SMN = 25;
 
 void solve(){
 	int n;
@@ -29,7 +32,7 @@ void solve(){
 signed main(){
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int tests=1;
-	//cin>>t;
+	//cin>>tests;
 	while(tests--)solve();
 }
 
