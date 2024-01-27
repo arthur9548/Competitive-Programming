2c42115b6f166b7fe4b03702a0600920064d1642
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-01-27 16:45:56 -0300

    Good dsu.cpp

--- Code/DataStructures/dsu.cpp
+++ Code/DataStructures/dsu.cpp
@@ -1,14 +1,20 @@
-int dsu[MXN];
-int level[MXN];
-
-int pai(int a){
-	if (dsu[a]==a)return a;
-	else return dsu[a] = pai(dsu[a]);
-}
-
-void merge(int a, int b){
-	int pa = pai(a), pb = pai(b);
-	if (pa==pb)return;
-	if (level[pb]>level[pa])swap(pa, pb);
-	dsu[pb] = pa; level[pa] += (level[pa]==level[pb]);
-}
+struct DSU {
+	vi cur_rep, level;
+	
+	DSU(int n){
+		cur_rep.resize(n); level.assign(n, 0);
+		rep(i, 0, n)cur_rep[i] = i;
+	}
+	
+	int find_rep(int el){
+		if (cur_rep[a]==a)return a;
+		else return cur_rep[a] = find_rep(cur_rep[a]);
+	}
+	
+	void merge(int a, int b){
+		a = find_rep(a); b = find_rep(b);
+		if (a==b)return;
+		if (level[b]>level[a])swap(a, b);
+		cur_rep[b] = a; level[a] += (level[a]==level[b]);
+	}
+};
