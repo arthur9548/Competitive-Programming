d19cace9ca36fd7bbe1ca1b0752eac12a9610eca
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-01-27 17:21:53 -0300

    Updated orderedset.cpp

--- Code/DataStructures/orderedset.cpp
+++ Code/DataStructures/orderedset.cpp
@@ -5,3 +5,10 @@ template<typename T, typename B = GNU::null_type>
 using ordered_set = GNU::tree<T, B, less<T>, GNU::rb_tree_tag, 
 GNU::tree_order_statistics_node_update>;
 
+void example(ordered_set<int> oset){
+  rep(i, 0, 5)if (i%2)oset.insert(i);
+  int pos = oset.order_of_key(4); //==2, number of smaller elements in set
+  int num = oset.find_by_order(2); //==5, acessing element by index
+}
+  
+  
