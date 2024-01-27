f05e18ee6890c400a418c10f856c53eae10ca007
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-01-27 16:38:05 -0300

    Good sparsetable.cpp

--- Code/DataStructures/sparsetable.cpp
+++ Code/DataStructures/sparsetable.cpp
@@ -1,24 +1,31 @@
-constexpr int LG = 25;
-
 int lg(int x){
-	return __builtin_clzll(1ll) - __builtin_clzll(x);
+	return __builtin_clzll(1ll) - __builtin_clzll((long long)x);
 }
 
-int sptable[LG][MXN];
-int v[MXN];
-int n;
+#define type int
+#define spop max
 
-void buildtable(){
-	rep(i, 0, n)sptable[0][i] = v[i];
-	rep(i, 1, LG){
-		rep(j, 0, n-(1ll<<(i-1))){
-			sptable[i][j] = min(sptable[i-1][j], sptable[i-1][j+(1ll<<(i-1))]);
+struct SpTable{
+	int n, logn;
+	vector<type> values;
+	vector<vector<type>> table;
+	
+	SpTable(vector<type> & v){
+		n = v.size(); logn = lg(n)+1;
+		values = v; table.assign(logn, vector<type>(n));
+		rep(i, 0, n)table[0][i] = values[i];
+		rep(i, 1, logn){
+			rep(j, 0, n-(1ll<<(i-1))){
+				table[i][j] = spop(table[i-1][j], table[i-1][j+(1ll<<(i-1))]);
+			}
 		}
 	}
+	
+	type query(int l, int r){
+		assert l<=r;
+		int k = lg(r-l+1);
+		return spop(table[k][l], table[k][r+1-(1ll<<k)]);
+	}
 }
-
-int query(int l, int r){
-	if (r < l)return 0;
-	int k = lg(r-l+1);
-	return min(sptable[k][l], sptable[k][r+1-(1ll<<k)]);
-}
+	
+	
