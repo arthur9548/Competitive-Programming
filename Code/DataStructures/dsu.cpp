5a63cf03cd91b788a7e3419a9fd849938895a50a
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-01-30 21:57:57 -0300

    Updated dsu.cpp

--- Code/DataStructures/dsu.cpp
+++ Code/DataStructures/dsu.cpp
@@ -1,12 +1,17 @@
+//DSU: structure to unite disjoint sets
+//Total time is ~O(n) (slightly worse)
 struct DSU {
-	vi cur_rep, level;
+	int components; //number of components
+	vi cur_rep, level; //representatives and level in dsu tree
+	vi szs; //size of each component
 	
 	DSU(int n){
-		cur_rep.resize(n); level.assign(n, 0);
+		cur_rep.resize(n); level.assign(n, 0); szs.assign(n, 1);
 		rep(i, 0, n)cur_rep[i] = i;
+		components = n;
 	}
 	
-	int find_rep(int el){
+	int find_rep(int a){
 		if (cur_rep[a]==a)return a;
 		else return cur_rep[a] = find_rep(cur_rep[a]);
 	}
@@ -15,6 +20,9 @@ struct DSU {
 		a = find_rep(a); b = find_rep(b);
 		if (a==b)return;
 		if (level[b]>level[a])swap(a, b);
-		cur_rep[b] = a; level[a] += (level[a]==level[b]);
+		cur_rep[b] = a; 
+		level[a] += (level[a]==level[b]);
+		szs[a] += szs[b];
+		components--;
 	}
 };
