afe7321174a0c62839df95c41cb1b4ffb53a6b79
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-02-01 14:26:59 -0300

    Updated dkstra.cpp

--- Code/Graphs/dkstra.cpp
+++ Code/Graphs/dkstra.cpp
@@ -1,22 +1,25 @@
-//Dijsktra: menores caminhos de todos os vertices em relação a
-//um vertice ou um conjunto deles (multisource)
-vi dkstra(int root, vector<vector<pii>> & g){
+//Dijsktra: shortest paths from source to all other vertices in O(logV(E+V))
+//Assumes positive weights in all edges
+//Can be adapted to creat shortest path DAG from root
+//Easily modified to be multisource
+vi dkstra(int source, vector<vector<pii>> & g){
 	int n = g.size();
-	vector<bool> visited(n, false);
+	vector<bool> visited(n, false); //vertices visited only once
 	vi dists(n, oo);
-	dists[root] = 0;
-	priority_queue<pii, vector<pii>, greater<pii>> pq;
-	pq.push({0, root});
+	dists[source] = 0;
+	priority_queue<pii, vector<pii>, greater<pii>> pq; //min heap
+	pq.push({0, source});
 	while(not pq.empty()){
 		auto p = pq.top(); pq.pop();
 		int v = p.second, d = p.first;
 		if (visited[v])continue;
 		visited[v] = true;
-		for(auto p2 : g[v]){
-			int adj=p2.first, nd=p2.second;
+		for(auto edge : g[v]){
+			int adj=edge.first, nd=edge.second;
 			if (dists[adj] > d+nd){ //optimizes memory and runtime
 				dists[adj] = d+nd;
 				pq.push({d+nd, adj});
+				//space to add the resulting edge in SPDAG
 			}
 		}
 	}
