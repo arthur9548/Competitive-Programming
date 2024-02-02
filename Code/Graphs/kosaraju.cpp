fb124b2b26cdf97e3bdbdc9b1aceb4304694c1e3
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-02-02 15:50:57 -0300

    Updated kosaraju.cpp

--- Code/Graphs/kosaraju.cpp
+++ Code/Graphs/kosaraju.cpp
@@ -1,31 +1,55 @@
-vector<vector<int>> g(MXN), gi(MXN);
-bool visited[MXN];
-int component[MXN];
-vector<int> order;
- 
-int n, m;
-
-void restart(){
-	rep(i, 0, n)visited[i] = false;
-}
-
-void dfs1(int v){
-	if (visited[v])return;
-	visited[v] = true;
-	for(int adj : g[v]) dfs1(adj);
-	order.pb(v);
-}
- 
-void dfs2(int v, int comp){
-	if (visited[v])return;
-	visited[v] = true;
-	for(int adj : gi[v])dfs2(adj, comp);
-	component[v] = comp;
-}
-
-void kosaraju(){
-	rep(i, 0, n)dfs1(i);
-	restart(); reverse(all(order));
-	for(int v : order)dfs2(v, v);
-	restart();
-}
+//Kosaraju: algorithm to get strongly connected components 
+//sorted by topological order in O(V+E)
+//Structure contains info on condensed graph and components
+struct Kosaraju{
+	int n, totalcomps; //number of strongly connected components
+	vvi g, gi; //graph structure
+	vvi cdg, cdgi; //condensed graph
+	vi component; //component of vertex (sorted by topological order)
+	vvi comps; //component list
+	
+	Kosaraju(vvi& og){
+		n = og.size(); totalcomps=0;
+		g.resize(n); gi.resize(n);
+		component.resize(n);
+		rep(i, 0, n){
+			for(int j : og[i]){
+				gi[j].pb(i); g[i].pb(j);
+			}
+		}
+		run(); //Kosaraju algorithm
+	}
+	
+	void run(){
+		vb visited(n, false);
+		vi order, curvs;
+		auto dfs1 = [&](auto self, int v){
+			if (visited[v])return;
+			visited[v] = true;
+			for(int adj : g[v])self(self, adj);
+			order.pb(v);
+		};
+		auto dfs2 = [&](auto self, int v, int curcomp){
+			if (visited[v])return;
+			visited[v] = true;
+			component[v] = curcomp; curvs.pb(v);
+			for(int adj : gi[v]){
+				if (visited[adj] and component[adj]!=curcomp){
+					int compadj = component[adj];
+					cdg[compadj].pb(curcomp); cdgi[curcomp].pb(compadj);
+				}
+				else self(self, adj, curcomp);
+			}
+		};
+		rep(i, 0, n)dfs1(dfs1, i);
+		rep(i, 0, n)visited[i] = false;
+		reverse(all(order));
+		rep(i, 0, n){
+			int v = order[i];
+			if (visited[v])continue;
+			cdg.pb(vi()); cdgi.pb(vi());
+			dfs2(dfs2, v, totalcomps); totalcomps++;
+			comps.pb(curvs); curvs.clear();
+		}
+	}
+};
