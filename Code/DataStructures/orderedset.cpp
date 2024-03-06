0bee257a02d449005a922ca5172c8bae4b0f5189
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-03-06 07:53:07 -0300

    Added ordered multiset

--- Code/DataStructures/orderedset.cpp
+++ Code/DataStructures/orderedset.cpp
@@ -10,5 +10,35 @@ void example(ordered_set<int> oset){
   int pos = oset.order_of_key(4); //==2, number of smaller elements in set
   int num = oset.find_by_order(2); //==5, acessing element by index
 }
+
+template<typename T>
+struct ordered_multiset{
+	ordered_set<pair<T, int>> oset;
+	int c;
+	ordered_multiset(){
+		c = 0;
+	}
+	int order_of_key(T data){
+		return oset.order_of_key({data, oo});
+	}
+	const T* find_by_order(int pos){
+		return &(*oset.find_by_order(pos)).first;
+	}
+	void insert(T data){
+		oset.insert({data, c++});
+	}
+	void erase(T data){
+		oset.erase(oset.lower_bound({data, 0}));
+	}
+	int size(){
+		return oset.size();
+	}
+  const T* lower_bound(T data){
+    return &(*oset.lower_bound({data, 0})).first;
+  }
+  const T* upper_bound(T data){
+    return &(*oset.upper_bound({data, oo})).first;
+  }
+};
   
   
