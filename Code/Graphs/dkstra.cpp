71f9df3ea9636b7059988ad94188cd57d60d4562
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-03-11 09:32:50 -0300

    Added dense dijkstra

--- Code/Graphs/dkstra.cpp
+++ Code/Graphs/dkstra.cpp
@@ -1,10 +1,10 @@
-//Dijsktra: shortest paths from source to all other vertices in O(logV(E+V))
+//Dijsktra: shortest paths from source to all other vertices in O(V + ElogV)
 //Assumes positive weights in all edges
 //Can be adapted to creat shortest path DAG from root
 //Easily modified to be multisource
 vi dkstra(int source, vector<vector<pii>> & g){
 	int n = g.size();
-	vector<bool> visited(n, false); //vertices visited only once
+	vb visited(n, false); //vertices visited only once
 	vi dists(n, oo);
 	dists[source] = 0;
 	priority_queue<pii, vector<pii>, greater<pii>> pq; //min heap
@@ -24,4 +24,34 @@ vi dkstra(int source, vector<vector<pii>> & g){
 		}
 	}
 	return dists; //default returning
+}
+
+//For dense graphs, a V*V approach is faster than ElogV, 
+//since E = O(V*V)
+vi densedkstra(int source, vector<vector<pii>> & g){
+	int n = g.size();
+	vb visited(n, false);
+	vi dists(n, oo);
+	dists[source] = 0;
+	auto get_min = [&]()->int{
+		int best = -1;
+		rep(v, 0, n){
+			if (visited[v])continue;
+			if (best==-1)best = v;
+			else if (dists[v] < dists[best])best = v;
+		}
+		return best;
+	};
+	int v = source;
+	while(v != -1){
+		visited[v] = true;
+		for(auto edge : g[v]){
+			int adj = edge.first, nd = edge.second;
+			if (dists[adj] > dists[v]+nd){
+				dists[adj] = dists[v]+nd;
+			}
+		}
+		v = get_min();
+	}
+	return dists;
 }
