6be98e7a81e747c827590e7a436f3befcf7c059e
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-05-23 13:02:22 -0300

    Updated template.cpp

--- Code/Misc/template.cpp
+++ Code/Misc/template.cpp
@@ -4,35 +4,36 @@ using namespace std;
 #define float long double
 #define esp ' '
 #define endl '\n'
-#define YES "YES\n"
-#define NO "NO\n"
-#define IMP "IMPOSSIBLE\n"
+#define YES "Yes\n"
+#define NO "No\n"
 #define vi vector<int>
-#define matrix vector<vector<int>>
+#define vvi vector<vector<int>>
+#define vbool vector<bool>
 #define pii pair<int, int>
 #define pb push_back
-#define ppb pop_back
 #define eb emplace_back
+#define ppb pop_back
 #define all(x) x.begin(), x.end()
 #define rep(i, a, b) for(int i = (int)(a); i < (int)(b); i++)
 #define repstep(i, a, b, s) for(int i = (int)(a); i < (int)(b); i+=(s))
 #define repinv(i, a, b) for(int i = (int)(a); i >= (int)(b); i--)
 #define debug(var) cout << #var << ": " << var << endl
 #define sz(x) (int)x.size()
-
-constexpr int oo = (((unsigned int)-1)>>4);
-constexpr int MOD = 1e9+7;
-constexpr int MXN = 2e5+2;
-constexpr int SMN = 25;
+ 
+constexpr int oo = (((unsigned int)-1)>>2);
+constexpr int MXN = 1e5+2;
+constexpr int MOD = 998244353;
+constexpr double PI = acos(-1);
 
 void solve(){
 	int n;
 }
 
 signed main(){
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0);
 	int tests=1;
 	//cin>>tests;
+	//cout << setprecision(12) << fixed;
 	while(tests--)solve();
 }
 
