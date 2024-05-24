a373de515485ef47b1bb9df16eb679af0b7ea16f
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-05-24 15:57:08 -0300

    Updated segtree.cpp

--- Code/DataStructures/segtree.cpp
+++ Code/DataStructures/segtree.cpp
@@ -2,63 +2,49 @@
 //easily customized: choose type of node, merge operation and query aspect
 //iteractive is about 1.2 faster than recursive segtree
 
-#define type int
-#define neutral oo
-
-type merge(type a, type b){
-	return min(a, b);
-}
-
-int parent(int pos){
-	return pos/2 - (1-pos%2);
-}
-
+#define parent(pos) ((pos/2)-1+(pos&1))
+template<typename type>
 struct SegTree{
+	using assoc_op = function<type(type, type)>;
 	int n; //seg n, not original n
 	vector<type> seg;
-	SegTree(vector<type> v){
+	assoc_op merge;
+	type neutral;
+	
+	SegTree(vector<type> v, assoc_op op, type iden){
 		int s = sz(v);
 		if (__builtin_popcount(s)==1) n = s;
 		else n = 1<<(1+__builtin_clz(1)-__builtin_clz(s));
+		neutral = iden; merge = op;
 		seg.assign(2*n-1, neutral);
 		rep(i, 0, s)update(i, v[i]);
 	}
 	
 	void update(int pos, type value){
-		pos += n-1;
-		seg[pos] = value;
+		pos += n-1; seg[pos] = value;
 		while(pos){
 			pos = parent(pos);
 			seg[pos] = merge(seg[2*pos+1], seg[2*pos+2]);
 		}
 	}
 	
-	//Interface function
 	type query(int l, int r){
-		//return iquery(l, r);
+		//return iquery(l, r); //if iteractive doesnt need interface
 		return rquery(l, r+1, 0, 0, n);
 	}
 	
-	//[l,r]
-	type iquery(int l, int r){
-		l+=n-1; r+=n-1;
-		type res = neutral;
+	type iquery(int l, int r){//[l,r]
+		l+=n-1; r+=n-1; type res = neutral;
 		while(l<=r){
-			if (l%2==0){ 
-			//l is right children, left brother should not be included in res
-				res = merge(res, seg[l]); l++;
-			}
-			if (r%2==1){
-			//r is left children, right brother should not be included in res
-				res = merge(res, seg[r]); r--;
-			}
+			if (l%2==0)res = merge(seg[l++], res);
+			if (r%2==1)res = merge(res, seg[r--]);
 			l = parent(l); r = parent(r);
 		}
 		return res;
 	}
 	
-	//[l,r[
-	type rquery(int l, int r, int pos, int lx, int rx){
+	
+	type rquery(int l, int r, int pos, int lx, int rx){//[l,r[
 		if (r <= lx or l >= rx)return neutral;
 		if (l <= lx and r >= rx)return seg[pos];
 		int mid = lx+(rx-lx)/2;
