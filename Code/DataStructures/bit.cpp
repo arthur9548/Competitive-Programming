b0b93fb4cd6cf8006aa086b762ccf9f56c55bd78
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-08 16:57:59 -0300

    State-of-art bit.cpp

--- Code/DataStructures/bit.cpp
+++ Code/DataStructures/bit.cpp
@@ -1,79 +1,33 @@
-//Binary Indexed Tree (BIT): structure to perform prefix queries with update
-//1-indexed!
-//O(logn) query and update, about 1.5x faster than recursive segtree
+//Title: Binary Indexed Tree (BIT)
+//Description: fast range query and point update
+//Restrictions: elements must be of a abelian group
+//Observations: 1-indexed!
+//Complexity: O(logn) query and update, about 1.5x faster than recursive segtree
+
+inline int lastbit(int pos){return pos&(-pos);}
+
+template<int dim, typename type>
 struct BIT{
-	int n; //BIT size
-	vi bit, nums; //BIT array, original array
-
-	BIT(int s){
-		n = s; bit.assign(n+1, 0); nums.assign(n+1, 0);
+	int n;
+	vector<BIT<dim-1, type>> bit;
+	template<typename... Args>
+	BIT(int s, Args... args):n(s+1),bit(n, BIT<dim-1, type>(args...)){}
+	template<typename...Args>
+	void add(int pos, Args... args){
+		for(pos++;pos<=n;pos+=lastbit(pos))bit[pos].add(args...);
 	}
-
-	BIT(vi v){
-		n = sz(v); bit.assign(n+1, 0); nums.assign(n+1, 0);
-		rep(i, 1, n+1)update(i, v[i-1]); //1-indexed
-	}
-
-	//Interface functions
-	void update(int pos, int x){
-		updt(pos, x-nums[pos]); nums[pos] = x;
-	}
-	
-	int query(int l, int r){
-		return qry(r)-qry(l-1);
-	}
-
-	//Real functions
-	void updt(int pos, int x){
-		while(pos <= n){
-			bit[pos]+=x; pos += (1ll<<(__builtin_ctzll(pos)));
-		}
-	}
-	
-	int qry(int pos){
-		int res = 0;
-		while(pos){
-			res += bit[pos]; pos -= (1ll<<(__builtin_ctzll(pos)));
-		}
+	template<typename...Args>
+	type query(int l, int r, Args... args){
+		type res(0); r++;
+		for(;r>=1;r-=lastbit(r))res+=bit[r].query(args...);
+		for(;l>=1;l-=lastbit(l))res-=bit[l].query(args...);
 		return res;
 	}
 };
 
-//BIT 2D: O(log^2 n query)
-struct BIT2D{
-	int n, m;
-	vector<BIT> bit;
-	vvi nums;
-	
-	BIT2D(int size1, int size2){
-		n = size1; m = size2;
-		nums.assign(n+1, vi(m+1, 0)); bit.assign(n+1, BIT(m)); //still 1-indexed
-	}
-
-	//Interface
-	void update(int x, int y, int num){
-		updt(x, y, num-nums[x][y]); nums[x][y] = num;
-	}
-	
-	int query(int x1, int y1, int x2, int y2){
-		x1--; y1--;
-		int res = qry(x2,y2)+qry(x1,y1);
-		res -= (qry(x1,y2)+qry(x2,y1));
-		return res;
-	}
-
-	//Real
-	void updt(int x, int y, int val){
-		while(x <= n){
-			bit[x].updt(y, val); x += (1ll<<(__builtin_ctzll(x)));
-		}
-	}
-	
-	int qry(int x, int y){
-		int res=0;
-		while(x){
-			res += bit[x].qry(y); x -= (1ll<<(__builtin_ctzll(x)));
-		}
-		return res;
-	}
+template<typename type>
+struct BIT<0, type>{
+	type val = type(0);
+	void add(type nval){val+=nval;}
+	type query(){return val;}
 };
