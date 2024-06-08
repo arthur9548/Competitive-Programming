d4ed4e5253057b5d25053c5a1033fd106c528bcf
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-08 17:05:21 -0300

    Updated bit.cpp

--- Code/DataStructures/bit.cpp
+++ Code/DataStructures/bit.cpp
@@ -1,15 +1,16 @@
 //Title: Binary Indexed Tree (BIT)
 //Description: fast range query and point update
-//Restrictions: elements must be of a abelian group
-//Observations: 1-indexed!
+//Restrictions: elements must be of a abelian group (+=, -=, 0)
+//Observations: 1-indexed only in implementation, interface is 0-indexed
 //Complexity: O(logn) query and update, about 1.5x faster than recursive segtree
+//Tested at: CSES-Dynamic Range Sum Queries and CSES-Forest Queries II
 
 inline int lastbit(int pos){return pos&(-pos);}
 
 template<int dim, typename type>
 struct BIT{
 	int n;
-	vector<BIT<dim-1, type>> bit;
+	vector<BIT<dim-1, type>> bit; //or map<int, BIT<dim-1, type>> if sparse
 	template<typename... Args>
 	BIT(int s, Args... args):n(s+1),bit(n, BIT<dim-1, type>(args...)){}
 	template<typename...Args>
@@ -31,3 +32,9 @@ struct BIT<0, type>{
 	void add(type nval){val+=nval;}
 	type query(){return val;}
 };
+
+void example(){
+	BIT<3, int> bit(3, 4, 5); //3D 3x4x5 bit
+	bit.add(0, 0, 0, 5); //added 5 at first point
+	cout << bit.query(0, 2, 0, 3, 0, 4) << endl; //query of whole bit
+}
