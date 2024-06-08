0f1c00d5486bce597b23b5c10cb4cf439594d42e
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-08 20:51:48 -0300

    Updated bit.cpp

--- Code/DataStructures/bit.cpp
+++ Code/DataStructures/bit.cpp
@@ -5,14 +5,13 @@
 //Complexity: O(logn) query and update, about 1.5x faster than recursive segtree
 //Tested at: CSES-Dynamic Range Sum Queries and CSES-Forest Queries II
 
-inline int lastbit(int pos){return pos&(-pos);}
-
 template<int dim, typename type>
 struct BIT{
 	int n;
 	vector<BIT<dim-1, type>> bit; //or map<int, BIT<dim-1, type>> if sparse
 	template<typename... Args>
 	BIT(int s, Args... args):n(s+1),bit(n, BIT<dim-1, type>(args...)){}
+	inline int lastbit(int pos){return pos&(-pos);}
 	template<typename...Args>
 	void add(int pos, Args... args){
 		for(pos++;pos<=n;pos+=lastbit(pos))bit[pos].add(args...);
