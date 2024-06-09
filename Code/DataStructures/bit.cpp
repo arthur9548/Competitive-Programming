2f651cc494b20be22a37c38a33494383893363f0
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-09 10:18:17 -0300

    Shorter bit.cpp

--- Code/DataStructures/bit.cpp
+++ Code/DataStructures/bit.cpp
@@ -5,31 +5,31 @@
 //Complexity: O(logn) query and update, about 1.5x faster than recursive segtree
 //Tested at: CSES-Dynamic Range Sum Queries and CSES-Forest Queries II
 
-template<int dim, typename type>
+template<int D, class T> 
 struct BIT{
 	int n;
-	vector<BIT<dim-1, type>> bit; //or map<int, BIT<dim-1, type>> if sparse
-	template<typename... Args>
-	BIT(int s, Args... args):n(s+1),bit(n, BIT<dim-1, type>(args...)){}
-	inline int lastbit(int pos){return pos&(-pos);}
-	template<typename...Args>
-	void add(int pos, Args... args){
-		for(pos++;pos<=n;pos+=lastbit(pos))bit[pos].add(args...);
+	vector<BIT<D-1, T>> bit;
+	template<class... A>
+	BIT(int s, A... ds):n(s),bit(n+1, BIT<D-1, T>(ds...)){}
+	inline int lastbit(int x){return x&(-x);}
+	template<class... A> 
+	void add(int pos, A... ps){
+		for(pos++;pos<=n;pos+=lastbit(pos))bit[pos].add(ps...);
 	}
-	template<typename...Args>
-	type query(int l, int r, Args... args){
-		type res(0); r++;
-		for(;r>=1;r-=lastbit(r))res+=bit[r].query(args...);
-		for(;l>=1;l-=lastbit(l))res-=bit[l].query(args...);
+	template<class... A>
+	T query(int l, int r, A... ps){
+		T res(0); r++;
+		for(;r>=1;r-=lastbit(r))res+=bit[r].query(ps...);
+		for(;l>=1;l-=lastbit(l))res-=bit[l].query(ps...);
 		return res;
 	}
 };
 
-template<typename type>
-struct BIT<0, type>{
-	type val = type(0);
-	void add(type nval){val+=nval;}
-	type query(){return val;}
+template<class T>
+struct BIT<0, T>{
+	T val = T(0);
+	void add(T nval){val+=nval;}
+	T query(){return val;}
 };
 
 void example(){
