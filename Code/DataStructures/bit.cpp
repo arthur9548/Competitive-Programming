3fd3cfe79cf2e140d1c8142ebe8a2bdfa42b40ba
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-09 11:02:02 -0300

    Example bit.cpp

--- Code/DataStructures/bit.cpp
+++ Code/DataStructures/bit.cpp
@@ -1,7 +1,7 @@
 //Title: multidimensional Binary Indexed Tree (BIT)
 //Description: fast range query and point update
 //Complexity: O(logn ^ D) query and update, O(n^D) memory
-//Restrictions: elements must be of a abelian group (+=, -=, 0)
+//Restrictions: elements must be of a abelian group (+=, -=, id)
 //Observations: 
 //--- 1-indexed only in implementation, interface is 0-indexed
 //--- Very good constant factor
@@ -20,7 +20,7 @@ struct BIT{
 	}
 	template<class... A>
 	T query(int l, int r, A... ps){
-		T res(0); r++;
+		T res = {T::id}; r++;
 		for(;r>=1;r-=lastbit(r))res+=bit[r].query(ps...);
 		for(;l>=1;l-=lastbit(l))res-=bit[l].query(ps...);
 		return res;
@@ -29,13 +29,19 @@ struct BIT{
 
 template<class T>
 struct BIT<0, T>{
-	T val = T(0);
+	T val = {T::id};
 	void add(T nval){val+=nval;}
 	T query(){return val;}
 };
 
+struct AG{ //abelian group analogous to int addition
+	int v; const static int id = 0;
+	AG& operator+=(const AG& o){v+=o.v;return *this;}
+	AG& operator-=(const AG& o){v-=o.v;return *this;}
+};
+
 void example(){
-	BIT<3, int> bit(3, 4, 5); //3D 3x4x5 bit
-	bit.add(0, 0, 0, 5); //added 5 at first point
-	cout << bit.query(0, 2, 0, 3, 0, 4) << endl; //query of whole bit
+	BIT<3, AG> bit(3, 4, 5); //3D 3x4x5 bit
+	bit.add(0, 0, 0, AG{5}); //added 5 at first point
+	cout << bit.query(0, 2, 0, 3, 0, 4).v << endl; //query of whole bit
 }
