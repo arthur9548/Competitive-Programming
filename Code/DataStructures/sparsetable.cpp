60db204de1ae68b3fc558602a970b747877d34ba
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-13 16:15:38 -0300

    State of art sparsetable.cpp

--- Code/DataStructures/sparsetable.cpp
+++ Code/DataStructures/sparsetable.cpp
@@ -1,35 +1,56 @@
-//Sparse Table: RMQ in O(nlogn) build, O(1) query
-//Customizable type and operations
+//Title: multidimensional Sparse Table
+//Description: static idempotent range queries
+//Complexity: O((nlogn)^D) build and memory, O(1) query
+//Restrictions: elements must be of an Idempotent Monoid (^, id)
+//Observations:
+//--- Query has a constant factor of 2^D
+//--- Don't forget to "init" the Sparse Table before querying!
+//Tested at: CSES-Static Range Mininum Queries and CF-Valiant's New Map (MLE)
 
-int lg2(int x){
-	return __builtin_clzll(1ll) - __builtin_clzll((long long)x);
-	// log2(x) in O(1)
-}
-
-#define type int
-#define spop max
-
+template<int D, class T>
 struct SpTable{
-	int n, logn;
-	vector<type> values; //original values, not needed besides constructor
-	vector<vector<type>> table;
-	
-	SpTable(vector<type> & v){
-		n = v.size(); logn = lg2(n)+1;
-		values = v; table.assign(logn, vector<type>(n));
-		rep(i, 0, n)table[0][i] = values[i];
-		rep(i, 1, logn){
-			rep(j, 0, n-(1ll<<(i-1))){
-				table[i][j] = spop(table[i-1][j], table[i-1][j+(1ll<<(i-1))]);
-			}
-		}
+	using itab = vector<SpTable<D-1, T>>;
+	int n;
+	vector<itab> tab;
+	inline int lg(signed x){return __builtin_clz(1)-__builtin_clz(x);}
+	template<class... A>
+	SpTable(int s, A... ds):n(s),
+	tab(1+lg(n),itab(n,SpTable<D-1, T>(ds...))){}
+	template<class... A>
+	void set(T x, int p, A... ps){tab[0][p].set(x, ps...);}
+	void join(auto a, auto b){
+		rep(i, 0, 1+lg(n))rep(j, 0, n)
+			tab[i][j].join(a.tab[i][j], b.tab[i][j]);
 	}
-	
-	type query(int l, int r){
-		assert l<=r;
-		int k = lg2(r-l+1);
-		return spop(table[k][l], table[k][r+1-(1ll<<k)]);
+	void init(){
+		rep(i, 0, n)tab[0][i].init();
+		rep(i, 0, lg(n))rep(j, 0, n-(1<<i))
+			tab[i+1][j].join(tab[i][j], tab[i][j+(1<<i)]);
+	}
+	template<class... A>
+	T query(int l, int r, A... ps){
+		int k = lg(r-l+1); r+=1-(1<<k);
+		return tab[k][l].query(ps...)^tab[k][r].query(ps...);
 	}
 };
-	
-	
+
+template<class T>
+struct SpTable<0, T>{
+	T val={T::id};
+	void set(T x){val=x;}
+	void join(auto a, auto b){val=a.val^b.val;}
+	void init(){}
+	T query(){return val;}
+};
+
+struct IM{
+	int v; static const int id = oo;
+	IM operator^(const IM & o){return {min(v,o.v)};}
+};
+
+void example(){
+	SpTable<3, IM> sp(4, 5, 3); //4x5x3 3D sparse table
+	sp.set(IM{1}, 0, 2, 1); //point (0,2,1) has now value 1
+	sp.init();
+	cout << sp.query(0, 3, 0, 4, 0, 2).v << endl; // = "1" by id value
+}
