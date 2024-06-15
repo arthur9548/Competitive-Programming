da642619eb85d4683abef9c622769e53b066a843
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-15 16:51:38 -0300

    Good sparsetable.cpp

--- Code/DataStructures/sparsetable.cpp
+++ Code/DataStructures/sparsetable.cpp
@@ -5,19 +5,18 @@
 //Observations:
 //--- Query has a constant factor of 2^D
 //--- Don't forget to "init" the Sparse Table before querying!
-//Tested at: CSES-Static Range Mininum Queries and CF-Valiant's New Map (MLE)
+//Tested at: CSES-Static Range Mininum Queries and CF-Animals and Puzzle
 
+#define MAs template<class...As> //multiple arguments
 template<int D, class T>
 struct SpTable{
 	using itab = vector<SpTable<D-1, T>>;
+	inline int lg(signed x){return __builtin_clz(1)-__builtin_clz(x);}
 	int n;
 	vector<itab> tab;
-	inline int lg(signed x){return __builtin_clz(1)-__builtin_clz(x);}
-	template<class... A>
-	SpTable(int s, A... ds):n(s),
+	MAs SpTable(int s, As... ds):n(s),
 	tab(1+lg(n),itab(n,SpTable<D-1, T>(ds...))){}
-	template<class... A>
-	void set(T x, int p, A... ps){tab[0][p].set(x, ps...);}
+	MAs void set(T x, int p, As... ps){tab[0][p].set(x, ps...);}
 	void join(auto a, auto b){
 		rep(i, 0, 1+lg(n))rep(j, 0, n)
 			tab[i][j].join(a.tab[i][j], b.tab[i][j]);
@@ -27,8 +26,7 @@ struct SpTable{
 		rep(i, 0, lg(n))rep(j, 0, n-(1<<i))
 			tab[i+1][j].join(tab[i][j], tab[i][j+(1<<i)]);
 	}
-	template<class... A>
-	T query(int l, int r, A... ps){
+	MAs T query(int l, int r, As... ps){
 		int k = lg(r-l+1); r+=1-(1<<k);
 		return tab[k][l].query(ps...)^tab[k][r].query(ps...);
 	}
