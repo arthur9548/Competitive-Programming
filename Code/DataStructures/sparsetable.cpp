07df0e6dc3dd1bb9fc0b21bbe7d420d1c7f223b9
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2024-06-19 12:04:07 -0300

    Friendly sparsetable.cpp

--- Code/DataStructures/sparsetable.cpp
+++ Code/DataStructures/sparsetable.cpp
@@ -8,16 +8,16 @@
 //Tested at: CSES-Static Range Mininum Queries and CF-Animals and Puzzle
 
 #define MAs template<class...As> //multiple arguments
-template<int D, class T>
-struct SpTable{
-	using itab = vector<SpTable<D-1, T>>;
+template<int D, class S>
+struct SpTable{ using T = typename S::T;
+	using isp = SpTable<D-1, S>;
 	inline int lg(signed x){return __builtin_clz(1)-__builtin_clz(x);}
 	int n;
-	vector<itab> tab;
+	vector<vector<isp>> tab;
 	MAs SpTable(int s, As... ds):n(s),
-	tab(1+lg(n),itab(n,SpTable<D-1, T>(ds...))){}
+	tab(1+lg(n),vector<isp>(n,isp(ds...))){}
 	MAs void set(T x, int p, As... ps){tab[0][p].set(x, ps...);}
-	void join(auto a, auto b){
+	void join(SpTable& a, SpTable& b){
 		rep(i, 0, 1+lg(n))rep(j, 0, n)
 			tab[i][j].join(a.tab[i][j], b.tab[i][j]);
 	}
@@ -28,27 +28,28 @@ struct SpTable{
 	}
 	MAs T query(int l, int r, As... ps){
 		int k = lg(r-l+1); r+=1-(1<<k);
-		return tab[k][l].query(ps...)^tab[k][r].query(ps...);
+		return S::op(tab[k][l].query(ps...),tab[k][r].query(ps...));
 	}
 };
 
-template<class T>
-struct SpTable<0, T>{
-	T val={T::id};
+template<class S>
+struct SpTable<0, S>{ using T = typename S::T;
+	T val=S::id;
 	void set(T x){val=x;}
-	void join(auto a, auto b){val=a.val^b.val;}
+	void join(SpTable& a, SpTable& b){val=S::op(a.val,b.val);}
 	void init(){}
 	T query(){return val;}
 };
 
 struct IM{
-	int v; static const int id = oo;
-	IM operator^(const IM & o){return {min(v,o.v)};}
+	using T = int;
+	static constexpr T id = oo;
+	static T op(T a, T b){return min(a, b);}
 };
 
 void example(){
 	SpTable<3, IM> sp(4, 5, 3); //4x5x3 3D sparse table
-	sp.set(IM{1}, 0, 2, 1); //point (0,2,1) has now value 1
+	sp.set(1, 0, 2, 1); //point (0,2,1) has now value 1
 	sp.init();
-	cout << sp.query(0, 3, 0, 4, 0, 2).v << endl; // = "1" by id value
+	cout << sp.query(0, 3, 0, 4, 0, 2) << endl; // = "1", because of id value
 }
