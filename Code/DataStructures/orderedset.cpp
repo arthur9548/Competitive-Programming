464c8197a735b533f69de29716aab6d3bb1e96b9
arthur9548 <tucobotelho@gmail.com>
2024-06-30 11:56:25 -0300

    Implemented ordered multiset

--- Code/DataStructures/orderedset.cpp
+++ Code/DataStructures/orderedset.cpp
@@ -1,44 +1,42 @@
-//does not run on C++ Windows 
+//Title: ordered/indexed set/multiset
+//Description: set with index access
+//Complexity: O(logn) accesses
+//Restrictions: elements must be comparable (< operator)
+//Observations:
+//--- worse constant compared to set (which means bad constant)
+//--- erases only one element by each erase call
+//--- does not run on C++ Windows
+//Tested at: CSES
+
 #include<bits/extc++.h> //include it before int long long
 #define GNU __gnu_pbds
-template<typename T, typename B = GNU::null_type>
+template<class T, class B = GNU::null_type>
 using ordered_set = GNU::tree<T, B, less<T>, GNU::rb_tree_tag, 
 GNU::tree_order_statistics_node_update>;
 
-void example(ordered_set<int> oset){
-  rep(i, 0, 6)if (i%2)oset.insert(i);
-  int pos = oset.order_of_key(4); //==2, number of smaller elements in set
-  int num = oset.find_by_order(2); //==5, acessing element by index
-}
-
-template<typename T>
+template<class T>
 struct ordered_multiset{
-	ordered_set<pair<T, int>> oset;
-	int c;
-	ordered_multiset(){
-		c = 0;
-	}
-	int order_of_key(T data){
-		return oset.order_of_key({data, oo});
-	}
-	const T* find_by_order(int pos){
-		return &(*oset.find_by_order(pos)).first;
-	}
-	void insert(T data){
-		oset.insert({data, c++});
-	}
-	void erase(T data){
-		oset.erase(oset.lower_bound({data, 0}));
-	}
-	int size(){
-		return oset.size();
-	}
-  const T* lower_bound(T data){
-    return &(*oset.lower_bound({data, 0})).first;
-  }
-  const T* upper_bound(T data){
-    return &(*oset.upper_bound({data, oo})).first;
-  }
+	ordered_set<pair<T, int>> o; int c;
+	ordered_multiset():c(0){}
+	const int order_of_key(T x){return o.order_of_key({x, -1});}
+	const T* find_by_order(int p){return &(*o.find_by_order(p)).first;}
+	void insert(T x){o.insert({x, c++});}
+	void erase(T x){o.erase(o.lower_bound({x, 0}));}
+	const unsigned size(){return o.size();}
+	const T* lower_bound(T x){return &(*o.lower_bound({x, 0})).first;}
+	const T* upper_bound(T x){return &(*o.upper_bound({x, c})).first;}
 };
   
-  
+void example(){
+	ordered_set<int> o1;
+	ordered_multiset<int> o2;
+	rep(i, 0, 3){
+		o1.insert(i); o2.insert(i); o2.insert(i);
+	}
+	cout << o1.size() << esp << o2.size() << endl; //3 6
+	int p1 = o1.order_of_key(1); //=1, one element smaller than 1
+	int p2 = o2.order_of_key(2); //=2
+	int x = *o1.find_by_order(2); //=2
+	int y = *o2.find_by_order(2); //=1
+	cout << x << esp << y << endl;
+}
