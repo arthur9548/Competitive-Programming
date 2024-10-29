0dfeaf1554182998acec1817939faf44ced9442a
arthur9548 <tucobotelho@gmail.com>
2024-10-29 16:44:12 -0300

    modifications in trie

--- Code/Graphs/kosaraju.cpp
+++ Code/Graphs/kosaraju.cpp
@@ -12,7 +12,7 @@ struct Kosaraju{
 	vector<vi> cdg, comps;
 	
 	Kosaraju(vector<vi>& g):n(sz(g)), cid(n, -1){
-		vector<vi> gi; rep(i, 0, n)for(int j : g[i])gi[j].pb(i);
+		vector<vi> gi(n); rep(i, 0, n)for(int j : g[i])gi[j].pb(i);
 		vi vis(n, 0), order;
 		auto dfs1 = [&](auto rec, int v)->void{
 			if (vis[v])return; 
@@ -28,13 +28,13 @@ struct Kosaraju{
 			comps.back().pb(v); 
 			for(int adj : gi[v]){
 				if (not vis[adj])rec(rec, adj);
-				else if (cid[adj]!=curid)cdg[cid[adj]].pb(curid);
+				else if (cid[adj]!=cid[v])cdg[cid[adj]].pb(cid[v]);
 			}
 		};
 		for(int v : order){
 			if (vis[v])continue;
 			cdg.pb(vi()); comps.pb(vi());
-			dfs2(dfs2, v, ++curid);
+			dfs2(dfs2, v);
 		}
 	}
 };
--- Code/Strings/trie.cpp
+++ Code/Strings/trie.cpp
@@ -12,3 +12,33 @@ struct Trie{
 		cnt[cur]++;
 	}
 };
+
+struct alphabet{
+	using T = char;
+	static constexpr int sz = 26;
+	static constexpr T inic = 'a';
+};
+
+template<class A>
+struct Trie{
+	using T = typename A::T;
+	vector<vector<int>> g; vi cnt;
+	Trie():g(1,vector<int>(A::sz,0)),cnt(1,0){}
+	int new_node(){g.pb(vector<int>(A::sz,0)); cnt.pb(0); return sz(g)-1;}
+	template<class S> void insert(const S & s){
+		int cur = 0;
+		for(T c : s){
+			if (g[cur][c-A::inic])cur = g[cur][c-A::inic];
+			else cur = g[cur][c-A::inic] = new_node();
+		}
+		cnt[cur]++;
+	}
+	template<class S> int count(const S & s){
+		int cur = 0;
+		for(T c : s){
+			if (g[cur][c-A::inic])cur = g[cur][c-A::inic];
+			else return 0;
+		}
+		return cnt[cur];
+	}
+};
